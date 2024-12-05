40b6d9d2ede6630b4bed29f77f7b8c44536f8220
SAI SAVERSH R <ssr1032005@gmail.com>
2024-12-05 14:18:21 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import SignupPage from "./components/SignupPage";
-import { useNavigate } from "react-router-dom";
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
-    </Router>
-  );
-};
-function App() 
-{
+function App() {
+  const navigate = useNavigate(); // Initialize the navigate function
+
+  const handleLoginPageNavigation = () => {
+    navigate("/login"); // Navigate to the LoginPage
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <h1 className='h2'>Askive</h1>
-          <button class="Get-Started" role="button">Login Page</button>
+          <button className="Get-Started" role="button" onClick={handleLoginPageNavigation}>
+          Get Started
+          </button>
         </p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default function RootApp() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
+}
